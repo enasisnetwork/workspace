59953ebd46d1fc61e15fae52a7d525902bd1d3cc
Robert Harris <30379153+enrh@users.noreply.github.com>
2023-12-28 23:56:44 +0000

    General updates to Makefile

--- makefile.py
+++ makefile.py
@@ -16,6 +16,8 @@ from re import MULTILINE
 from re import findall as re_findall
 from re import sub as re_sub
 from sys import stdout
+from typing import get_args
+from typing import Literal
 from typing import Optional
 
 
@@ -26,25 +28,53 @@ VERSION = '1.0.1'
 
 COLOR = environ.get('COLOR', 7)
 
+PREFICES = Literal[
+    'text', 'base', 'more']
+
 
 
 def makeout(
     string: str,
+    prefix: Optional[PREFICES] = None,
 ) -> None:
     """
     Print the ANSI colorized string to the standard output.
 
+    .. note::
+       This function is forgiving due to use with Makefile.
+
     :param string: String processed using inline directives.
+    :param prefix: Determine if and which prefix prepended.
     """
+
+    pattern = r'\<c([\d\;]+)\>'
+    replace = r'\033[0;\1m'
+
+
+    if prefix:
+
+        string = f'{string.lstrip(" ")}\n'
+
+        padding = 3
+        _prefix = ''
+
+        if prefix == 'base':
+            padding = 0
+            _prefix = '<cL>>>><c0>'
+
+        elif prefix == 'more':
+            padding = 2
+            _prefix = '<cL>●<c0>'
+
+        string = (
+            f'{" " * padding}'
+            f'{_prefix} {string}')
+
 
     string = (
         string
         .replace('<cD>', f'<c3{COLOR}>')
-        .replace('<cL>', f'<c9{COLOR}>')
-        .replace('<cZ>', '<c0>'))
-
-    pattern = r'\<c([\d\;]+)\>'
-    replace = r'\033[0;\1m'
+        .replace('<cL>', f'<c9{COLOR}>'))
 
     stdout.write(
         re_sub(pattern, replace, string))
@@ -78,16 +108,20 @@ def makefile(
     contents = makeread(path)
 
     pattern = (
-        r'^(\S+):(\s+[\.\S+\-]+)?\n'
-        r'\s+\@\#\#\s+([^\n]+)$')
+        r'\n([a-z]\S+)\:(\s+\\\n)?'
+        r'((\s+[^\n]+){1,2})?\n'
+        r'\s+\@##\s([^\n]+)?\n')
 
     matches = re_findall(
         pattern, contents, MULTILINE)
 
+    if len(matches) == 0:
+        return
+
     for match in matches:
         makeout(
             f'  <c9{COLOR}>{match[0]}'
-            f'  <c0>{match[2]}\n')
+            f'  <c0>{match[4]}\n')
 
 
 
@@ -96,9 +130,10 @@ def children() -> None:
     Locate and enumerate Makefiles from recipe directories.
     """
 
-    recipes = sorted(
+    makefiles = sorted(
         glob('Recipes/*/*.mk')
-        + glob('Recipes/*.mk'))
+        + glob('Recipes/*.mk')
+        + glob('workspace.mk'))
 
 
     def _value(
@@ -107,65 +142,87 @@ def children() -> None:
 
         matches = re_findall(
             rf'^{key} \+?= ([^\n]+)',
-            contents,
+            content,
             MULTILINE)
 
         if len(matches) == 0:
             return None
 
-        assert len(matches) == 1
-
-        return str(matches[0])
+        return str(matches[-1])
 
 
     def _print(
         recipe: str,
         about: str,
     ) -> None:
-
         makeout(
             f'  <c9{COLOR}>{recipe}'
-            f' <c0>{about}\n')
+            f'  <c0>{about}\n')
 
 
-    for recipe in recipes:
-        contents = makeread(recipe)
-        pkey = _value('WKSP_PROJKEY')
-        base = _value(f'WKSP_{pkey}_BASE')
-        path = _value(f'WKSP_{pkey}_PATH')
+    def _keys(
+        keys: str,
+    ) -> list[str]:
+        return (
+            re_sub(r'\s{2,}', ' ', keys)
+            .split(' '))
+
+
+    for file in makefiles:
+        content = makeread(file)
+
+        base = _value('WKSP_PROJKEY')
         make = _value('WKSP_MEXTEND')
-        gitr = _value(f'WKSP_{pkey}_GITR')
-        gitb = _value(f'WKSP_{pkey}_GITB')
 
-        makeout(
-            f'\n <c90>{base}/'
-            f'<c37>{path}<cZ>\n')
+        keys: set[str] = set()
 
-        if make == pkey:
-            _print(
-                f'{path}-make',
-                'Interact with the'
-                ' makefile recipes')
+        dynamics: dict[str, dict] = {}
 
-        if gitr is not None:
-            _print(
-                f'{path}-clone',
-                'Clone the project'
-                ' Git repository')
+        if base is not None:
+            keys.update(_keys(base))
 
-        if gitr or gitb:
-            _print(
-                f'{path}-git',
-                'Manage the project'
-                ' Git repository')
+        if make is not None:
+            keys.update(_keys(make))
 
-        if gitr is not None:
-            _print(
-                f'{path}-remove',
-                'Remove the project'
-                ' Git repository')
+        for key in keys:
 
-        makefile(recipe)
+            base = _value(f'WKSP_{key}_BASE')
+            path = _value(f'WKSP_{key}_PATH')
+            gitr = _value(f'WKSP_{key}_GITR')
+            gitb = _value(f'WKSP_{key}_GITB')
+
+            if not base and not path:
+                continue
+
+            makeout(
+                f'\n <c90>{base}/'
+                f'<c37>{path}<c0>\n')
+
+            if make == key:
+                _print(
+                    f'{path}-make',
+                    'Interact with the'
+                    ' makefile recipes')
+
+            if gitr is not None:
+                _print(
+                    f'{path}-clone',
+                    'Clone the project'
+                    ' Git repository')
+
+            if gitr or gitb:
+                _print(
+                    f'{path}-git',
+                    'Manage the project'
+                    ' Git repository')
+
+            if gitr is not None:
+                _print(
+                    f'{path}-remove',
+                    'Remove the project'
+                    ' Git repository')
+
+        makefile(file)
 
 
 
