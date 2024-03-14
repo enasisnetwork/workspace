a39163d17052b8bcc23fae88b353facf8e4c8a4b
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-03-14 03:44:52 +0000

    Add new recipe for enstatic

---
+++ Recipes/enstatic.mk
@@ -0,0 +1,14 @@
+# Operation recipes for managing the projects and execution environment.
+#
+# This file is part of Enasis Network software eco-system. Distribution
+# is permitted, for more information consult the project license file.
+
+
+
+WKSP_PROJKEY += enstatic
+WKSP_MEXTEND += enstatic
+
+WKSP_enstatic_BASE = Projects
+WKSP_enstatic_PATH = enstatic
+WKSP_enstatic_GITR = git@github.com:enasisnetwork/enstatic.git
+#WKSP_enstatic_GITB
