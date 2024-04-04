3b73d407393fe2c038f5514f6333cde661085f82
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-04-04 22:19:27 +0000

    Add new recipe for enhomie

---
+++ Recipes/enhomie.mk
@@ -0,0 +1,14 @@
+# Operation recipes for managing the projects and execution environment.
+#
+# This file is part of Enasis Network software eco-system. Distribution
+# is permitted, for more information consult the project license file.
+
+
+
+WKSP_PROJKEY += enhomie
+WKSP_MEXTEND += enhomie
+
+WKSP_enhomie_BASE = Projects
+WKSP_enhomie_PATH = enhomie
+WKSP_enhomie_GITR = git@github.com:enasisnetwork/enhomie.git
+#WKSP_enhomie_GITB
