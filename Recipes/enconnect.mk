70c62b4a4ae293afc43461b37e2b309dfb620fbd
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-04-25 02:59:31 +0000

    Add new recipe for enconnect (#12)

---
+++ Recipes/enconnect.mk
@@ -0,0 +1,14 @@
+# Operation recipes for managing the projects and execution environment.
+#
+# This file is part of Enasis Network software eco-system. Distribution
+# is permitted, for more information consult the project license file.
+
+
+
+WKSP_PROJKEY += enconnect
+WKSP_MEXTEND += enconnect
+
+WKSP_enconnect_BASE = Projects
+WKSP_enconnect_PATH = enconnect
+WKSP_enconnect_GITR = git@github.com:enasisnetwork/enconnect.git
+#WKSP_enconnect_GITB
