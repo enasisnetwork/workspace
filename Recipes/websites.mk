eed4959ab804fa521e8ebac6a649e51722161d77
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-09-04 02:28:10 +0000

    Add new recipe for websites (#16)

---
+++ Recipes/websites.mk
@@ -0,0 +1,14 @@
+# Operation recipes for managing the projects and execution environment.
+#
+# This file is part of Enasis Network software eco-system. Distribution
+# is permitted, for more information consult the project license file.
+
+
+
+WKSP_PROJKEY += websites
+WKSP_MEXTEND += websites
+
+WKSP_websites_BASE = Projects
+WKSP_websites_PATH = websites
+WKSP_websites_GITR = git@github.com:enasisnetwork/websites.git
+#WKSP_websites_GITB
