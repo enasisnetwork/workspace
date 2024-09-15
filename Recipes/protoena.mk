3a69115971478fb540d3a566a8c6f333d063101c
Robert Harris <30379153+enrh@users.noreply.github.com>
2024-09-15 15:03:06 +0000

    Basic minor improvements

---
+++ Recipes/protoena.mk
@@ -0,0 +1,14 @@
+# Operation recipes for managing the projects and execution environment.
+#
+# This file is part of Enasis Network software eco-system. Distribution
+# is permitted, for more information consult the project license file.
+
+
+
+WKSP_PROJKEY += protoena
+WKSP_MEXTEND += protoena
+
+WKSP_prototypes_BASE = Projects
+WKSP_prototypes_PATH = protoena
+WKSP_prototypes_GITR = git@github.com:enasisnetwork/protoena.git
+#WKSP_prototypes_GITB
