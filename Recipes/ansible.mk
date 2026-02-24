9a62c72544cd21c0765cdc6cb30783cfc57004d6
Robert Harris <30379153+enrh@users.noreply.github.com>
2026-02-24 06:21:36 +0000

    Update recipes for Ansible (#39)

--- Recipes/ansible.mk
+++ Recipes/ansible.mk
@@ -7,45 +7,94 @@
 
 WKSP_PROJKEY += \
 	ansbutility \
+	ansbutilitystatic \
 	ansbprojects \
+	ansbprojectsstatic \
 	ansbcertauth \
+	ansbcertauthstatic \
 	ansbprovision \
+	ansbprovisionstatic \
 	ansbdomain \
+	ansbdomainstatic \
 	ansbappstack \
-	ansbinternal
+	ansbappstackstatic \
+	ansbinternal \
+	ansbinternalstatic
 WKSP_MEXTEND += \
 	ansbutility \
+	ansbutilitystatic \
 	ansbprojects \
+	ansbprojectsstatic \
 	ansbcertauth \
+	ansbcertauthstatic \
 	ansbprovision \
+	ansbprovisionstatic \
 	ansbdomain \
+	ansbdomainstatic \
 	ansbappstack \
-	ansbinternal
+	ansbappstackstatic \
+	ansbinternal \
+	ansbinternalstatic
 
 WKSP_ansbutility_BASE = Projects
 WKSP_ansbutility_PATH = ansible-utility
 WKSP_ansbutility_GITR = git@github.com:enasisnetwork/ansible-utility.git
 
+WKSP_ansbutilitystatic_BASE = Projects
+WKSP_ansbutilitystatic_PATH = ansible-utility-static
+WKSP_ansbutilitystatic_GITR = git@github.com:enasisnetwork/ansible-utility.git
+WKSP_ansbutilitystatic_GITB = static
+
 WKSP_ansbprojects_BASE = Projects
 WKSP_ansbprojects_PATH = ansible-projects
 WKSP_ansbprojects_GITR = git@github.com:enasisnetwork/ansible-projects.git
+
+WKSP_ansbprojectsstatic_BASE = Projects
+WKSP_ansbprojectsstatic_PATH = ansible-projects-static
+WKSP_ansbprojectsstatic_GITR = git@github.com:enasisnetwork/ansible-projects.git
+WKSP_ansbprojectsstatic_GITB = static
 
 WKSP_ansbcertauth_BASE = Projects
 WKSP_ansbcertauth_PATH = ansible-certauth
 WKSP_ansbcertauth_GITR = git@github.com:enasisnetwork/ansible-certauth.git
 
+WKSP_ansbcertauthstatic_BASE = Projects
+WKSP_ansbcertauthstatic_PATH = ansible-certauth-static
+WKSP_ansbcertauthstatic_GITR = git@github.com:enasisnetwork/ansible-certauth.git
+WKSP_ansbcertauthstatic_GITB = static
+
 WKSP_ansbprovision_BASE = Projects
 WKSP_ansbprovision_PATH = ansible-provision
 WKSP_ansbprovision_GITR = git@github.com:enasisnetwork/ansible-provision.git
+
+WKSP_ansbprovisionstatic_BASE = Projects
+WKSP_ansbprovisionstatic_PATH = ansible-provision-static
+WKSP_ansbprovisionstatic_GITR = git@github.com:enasisnetwork/ansible-provision.git
+WKSP_ansbprovisionstatic_GITB = static
 
 WKSP_ansbdomain_BASE = Projects
 WKSP_ansbdomain_PATH = ansible-domain
 WKSP_ansbdomain_GITR = git@github.com:enasisnetwork/ansible-domain.git
 
+WKSP_ansbdomainstatic_BASE = Projects
+WKSP_ansbdomainstatic_PATH = ansible-domain-static
+WKSP_ansbdomainstatic_GITR = git@github.com:enasisnetwork/ansible-domain.git
+WKSP_ansbdomainstatic_GITB = static
+
 WKSP_ansbappstack_BASE = Projects
 WKSP_ansbappstack_PATH = ansible-appstack
 WKSP_ansbappstack_GITR = git@github.com:enasisnetwork/ansible-appstack.git
 
+WKSP_ansbappstackstatic_BASE = Projects
+WKSP_ansbappstackstatic_PATH = ansible-appstack-static
+WKSP_ansbappstackstatic_GITR = git@github.com:enasisnetwork/ansible-appstack.git
+WKSP_ansbappstackstatic_GITB = static
+
 WKSP_ansbinternal_BASE = Projects
 WKSP_ansbinternal_PATH = ansible-internal
 WKSP_ansbinternal_GITR = git@github.com:enasisnetwork/ansible-internal.git
+
+WKSP_ansbinternalstatic_BASE = Projects
+WKSP_ansbinternalstatic_PATH = ansible-internal-static
+WKSP_ansbinternalstatic_GITR = git@github.com:enasisnetwork/ansible-internal.git
+WKSP_ansbinternalstatic_GITB = static
